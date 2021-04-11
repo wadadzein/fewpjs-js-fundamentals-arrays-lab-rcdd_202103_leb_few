f880b34d6dacff7d80b133a3e83ff8429b2d0c50
wadad zein <wadad_zein@hotmail.com>
2021-04-11 15:56:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const pets = ["Milo", "Otis", "Garfield"];
+const pets =["Milo", "Otis", "Garfield"];
 const append = pets.push(Odie);
 const prepend = pets.unshift(Odie);
 const removeLast = pets.pop();
