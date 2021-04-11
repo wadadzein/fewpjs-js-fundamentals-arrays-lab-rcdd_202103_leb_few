d68ce279a6a9870523247a5ddefc776df1db290d
wadad zein <wadad_zein@hotmail.com>
2021-04-11 16:00:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const pets =["Milo", "Otis", "Garfield"];
-const append= pets.push(Odie);
-const prepend = pets.unshift(Odie);
-const removeLast = pets.pop();
-const removeFirst = pets.shift();
+
+const append= ["Milo", "Otis", "Garfield"];
+append.push(Odie
+const prepend = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
